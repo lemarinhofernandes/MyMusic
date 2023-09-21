dfb2e319d071c864882a6a095e550753e0ae7568
Luís Eduardo Marinho Fernandes <duduzoeras@MacBook-Air-de-Luis.local>
2023-09-20 22:47:51 -0300

    add initial table with data

---
+++ MyMusic/Model/Song.swift
@@ -0,0 +1,14 @@
+//
+//  Song.swift
+//  MyMusic
+//
+//  Created by Luís Eduardo Marinho Fernandes on 20/09/23.
+//
+
+struct Song {
+    let name: String
+    let albumName: String
+    let artistName: String
+    let imageName: String
+    let trackName: String
+}
--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -8,12 +8,100 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    var songs = [Song]()
+    
+    @IBOutlet var table: UITableView!
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureSongs()
+        table.delegate = self
+        table.dataSource = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
-
+    
+    //MARK: - Functions
+    func configureSongs() {
+        songs.append(Song(name: "Background music",
+                          albumName: "123 Other",
+                          artistName: "Rnado",
+                          imageName: "cover1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Havana",
+                          albumName: "Havana album",
+                          artistName: "Camilla Cabello",
+                          imageName: "cover2",
+                          trackName: "song2"))
+        songs.append(Song(name: "Viva La Vida",
+                          albumName: "123 Something",
+                          artistName: "Coldplay",
+                          imageName: "cover3",
+                          trackName: "song3"))
+        songs.append(Song(name: "Background music",
+                          albumName: "123 Other",
+                          artistName: "Rnado",
+                          imageName: "cover1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Havana",
+                          albumName: "Havana album",
+                          artistName: "Camilla Cabello",
+                          imageName: "cover2",
+                          trackName: "song2"))
+        songs.append(Song(name: "Viva La Vida",
+                          albumName: "123 Something",
+                          artistName: "Coldplay",
+                          imageName: "cover3",
+                          trackName: "song3"))
+        songs.append(Song(name: "Background music",
+                          albumName: "123 Other",
+                          artistName: "Rnado",
+                          imageName: "cover1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Havana",
+                          albumName: "Havana album",
+                          artistName: "Camilla Cabello",
+                          imageName: "cover2",
+                          trackName: "song2"))
+        songs.append(Song(name: "Viva La Vida",
+                          albumName: "123 Something",
+                          artistName: "Coldplay",
+                          imageName: "cover3",
+                          trackName: "song3"))
+    }
+    
 }
 
+//MARK: - Extensions
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        let song = songs[indexPath.row]
+        // configure
+        cell.textLabel?.text = song.name
+        cell.detailTextLabel?.text = song.albumName
+        cell.accessoryType = .disclosureIndicator
+        cell.imageView?.image = UIImage(named: "\(song.imageName).jpg")
+        cell.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 18)
+        cell.detailTextLabel?.font = UIFont(name: "Helvetica", size: 17)
+
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    } 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let position = indexPath.row
+        
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "player") else { return }
+        present(vc, animated: true)
+    }
+}

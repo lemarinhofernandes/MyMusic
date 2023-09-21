a2a25f5158b632eb522f3f2868bdf3a6c999567c
Luís Eduardo Marinho Fernandes <duduzoeras@MacBook-Air-de-Luis.local>
2023-09-20 21:26:14 -0300

    init

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Luís Eduardo Marinho Fernandes on 20/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
